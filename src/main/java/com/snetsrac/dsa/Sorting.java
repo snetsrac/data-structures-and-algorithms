608fdd8905524fea7540b83863c35e2d25412b15
snetsrac <dev@chriscarstens.com>
2021-11-22 03:00:40 +0000

    Module 11: Divide & Conquer Sorts

--- src/main/java/com/snetsrac/dsa/Sorting.java
+++ src/main/java/com/snetsrac/dsa/Sorting.java
@@ -1,9 +1,11 @@
 package com.snetsrac.dsa;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.LinkedList;
 
 /**
- * Your implementation of various iterative sorting algorithms.
+ * Your implementation of various iterative and divide & conquer sorting algorithms.
  */
 public class Sorting {
 
@@ -95,6 +97,156 @@ public class Sorting {
             while (i > 0 && comparator.compare(arr[i - 1], arr[i]) > 0) {
                 swap(arr, i - 1, i);
                 i--;
+            }
+        }
+    }
+
+    /**
+     * Implement merge sort.
+     *
+     * It should be:
+     * out-of-place
+     * stable
+     * not adaptive
+     *
+     * Have a worst case running time of: O(n log n)
+     * And a best case running time of: O(n log n)
+     *
+     * You can create more arrays to run merge sort, but at the end, everything
+     * should be merged back into the original T[] which was passed in.
+     *
+     * When splitting the array, if there is an odd number of elements, put the
+     * extra data on the right side.
+     *
+     * Hint: You may need to create a helper method that merges two arrays
+     * back into the original T[] array. If two data are equal when merging,
+     * think about which subarray you should pull from first.
+     *
+     * You may assume that the passed in array and comparator are both valid
+     * and will not be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array to be sorted.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        if (arr.length <= 1) {
+            return;
+        }
+
+        int midpoint = arr.length / 2;
+        T[] left = (T[]) new Object[midpoint];
+        T[] right = (T[]) new Object[arr.length - midpoint];
+    
+        for (int i = 0; i < midpoint; i++) {
+            left[i] = arr[i];
+        }
+
+        for (int i = midpoint; i < arr.length; i++) {
+            right[i - midpoint] = arr[i];
+        }
+
+        mergeSort(left, comparator);
+        mergeSort(right, comparator);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < left.length && j < right.length) {
+            if (comparator.compare(left[i], right[j]) <= 0) {
+                arr[i + j] = left[i];
+                i++;
+            } else {
+                arr[i + j] = right[j];
+                j++;
+            }
+        }
+
+        while (i < left.length) {
+            arr[i + j] = left[i];
+            i++;
+        }
+
+        while (j < right.length) {
+            arr[i + j] = right[j];
+            j++;
+        }
+    }
+
+    /**
+     * Implement LSD (least significant digit) radix sort.
+     *
+     * It should be:
+     * out-of-place
+     * stable
+     * not adaptive
+     *
+     * Have a worst case running time of: O(kn)
+     * And a best case running time of: O(kn)
+     *
+     * Feel free to make an initial O(n) passthrough of the array to
+     * determine k, the number of iterations you need.
+     *
+     * At no point should you find yourself needing a way to exponentiate a
+     * number; any such method would be non-O(1). Think about how how you can
+     * get each power of BASE naturally and efficiently as the algorithm
+     * progresses through each digit.
+     *
+     * You may use an ArrayList or LinkedList if you wish, but it should only
+     * be used inside radix sort and any radix sort helpers. Do NOT use these
+     * classes with merge sort. However, be sure the List implementation you
+     * choose allows for stability while being as efficient as possible.
+     *
+     * Do NOT use anything from the Math class except Math.abs().
+     *
+     * You may assume that the passed in array is valid and will not be null.
+     *
+     * @param arr The array to be sorted.
+     */
+    @SuppressWarnings("unchecked")
+    public static void lsdRadixSort(int[] arr) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        int absMax = 0;
+
+        for (int num : arr) {
+            // Handle edge case of minimum possible signed int, the absolute
+            // value of which cannot be stored in an int
+            if (num == Integer.MIN_VALUE) {
+                absMax = Integer.MAX_VALUE;
+                break;
+            } else if (Math.abs(num) > absMax) {
+                absMax = Math.abs(num);
+            }
+        }
+
+        int tenToTheK = 1;
+        absMax /= 10;
+
+        while (absMax != 0) {
+            tenToTheK *= 10;
+            absMax /= 10;
+        }
+
+        List<Integer>[] buckets = (LinkedList<Integer>[]) new LinkedList[19];
+
+        for (int i = 0; i < 19; i++) {
+            buckets[i] = new LinkedList<Integer>();
+        }
+
+        for (int digitPlace = 1; digitPlace <= tenToTheK; digitPlace *= 10) {
+            for (int num : arr) {
+                int digit = (num / digitPlace) % 10;
+                buckets[digit + 9].add(num);
+            }
+
+            int i = 0;
+
+            for (List<Integer> bucket : buckets) {
+                while (!bucket.isEmpty()) {
+                    arr[i++] = bucket.remove(0);
+                }
             }
         }
     }
--- src/test/java/com/snetsrac/dsa/SortingTest.java
+++ src/test/java/com/snetsrac/dsa/SortingTest.java
@@ -14,6 +14,7 @@ public class SortingTest {
 
     @BeforeEach
     public void setUp() {
+        sorted = new Integer[] { 1, 2, 3, 4, 5, 6, 7 };
         arr = new Integer[] { 4, 3, 1, 5, 2, 6, 7 };
         comparator = new TestComparator<>();
     }
@@ -22,8 +23,6 @@ public class SortingTest {
 
     @Test
     public void bubbleSortSorts() {
-        sorted = new Integer[] { 1, 2, 3, 4, 5, 6, 7 };
-
         Sorting.bubbleSort(arr, comparator);
 
         assertArrayEquals(sorted, arr);
@@ -34,8 +33,6 @@ public class SortingTest {
 
     @Test
     public void selectionSortSorts() {
-        sorted = new Integer[] { 1, 2, 3, 4, 5, 6, 7 };
-
         Sorting.selectionSort(arr, comparator);
 
         assertArrayEquals(sorted, arr);
@@ -51,13 +48,13 @@ public class SortingTest {
         Sorting.insertionSort(arr, comparator);
 
         assertArrayEquals(sorted, arr);
-        assertEquals(9, comparator.getCount());
+        assertEquals(10, comparator.getCount());
     }
 
     @Test
     public void insertionSortSorts2() {
         sorted = new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        arr = new Integer[] { 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1};
+        arr = new Integer[] { 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1 };
 
         Sorting.insertionSort(arr, comparator);
 
@@ -72,6 +69,48 @@ public class SortingTest {
         Sorting.insertionSort(arr, comparator);
 
         assertArrayEquals(sorted, arr);
+    }
+
+    // mergeSort()
+
+    @Test
+    public void mergeSortSorts() {
+        Sorting.mergeSort(arr, comparator);
+
+        assertArrayEquals(arr, sorted);
+        assertEquals(11, comparator.getCount());
+    }
+
+    // lsdRadixSort()
+
+    @Test
+    public void lsdRadixSortSorts() {
+        int[] radixSorted = new int[] { -9403, -207, -92, -1, 53, 231, 634, 1000, 2014 };
+        int[] radixArr = new int[] { 1000, 2014, 231, 53, -1, -92, -9403, 634, -207 };
+
+        Sorting.lsdRadixSort(radixArr);
+        
+        assertArrayEquals(radixSorted, radixArr);
+    }
+
+    @Test
+    public void lsdRadixSortSorts2() {
+        int[] radixSorted = new int[] { -2147483648, -800, -796, -785, -138, 104, 151, 258, 543, 908 };
+        int[] radixArr = new int[] { 908, 543, 258, 151, 104, -138, -785, -796, -800, -2147483648 };
+
+        Sorting.lsdRadixSort(radixArr);
+        
+        assertArrayEquals(radixSorted, radixArr);
+    }
+
+    @Test
+    public void lsdRadixSortSorts3() {
+        int[] radixSorted = new int[] { -2147483648, -2147483648, -9 };
+        int[] radixArr = new int[] { -9, -2147483648, -2147483648 };
+
+        Sorting.lsdRadixSort(radixArr);
+        
+        assertArrayEquals(radixSorted, radixArr);
     }
 
     private class TestComparator<T extends Comparable<? super T>> implements Comparator<T> {
